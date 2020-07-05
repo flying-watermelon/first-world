25da2c6b4480b57a12a34d09f95f46818bb095af
Yang Zhang <yangzhang1908@gmail.com>
2020-07-05 22:09:17 +0200

    add breeding module for plants

--- first-world/Logic/Breeding.cs
+++ first-world/Logic/Breeding.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Numerics;
 using FirstWorld.Model;
 
 namespace FirstWorld.Logic
@@ -6,11 +8,27 @@ namespace FirstWorld.Logic
     {
         public bool CanApply(IObject obj, World world)
         {
-            return true;
+             return (obj is Plant plant)
+                 && plant.Age>=plant.BreedingAge 
+                 && (plant.Age-plant.BreedingAge)%plant.BreedingPeriod ==0;
         }
 
         public void Apply(IObject obj, World world)
         {
+            if(obj is Plant plant)
+            {
+                var rand = new Random();
+                float randomValue = Convert.ToSingle(rand.NextDouble());
+                Plant childPlant = new Plant();
+                childPlant.Age = 0;
+/*                 childPlant.Position= new Vector3(Convert.ToSingle(plant.Position.X + Math.Sin(randomValue)*plant.SeedSpreadRadius),
+                                                 Convert.ToSingle(plant.Position.Y + Math.Cos(randomValue)*plant.SeedSpreadRadius),
+                                                 0.0f); */
+                childPlant.Position= new Vector3((float)(plant.Position.X + Math.Sin(randomValue)*plant.SeedSpreadingRadius),
+                                                 (float)(plant.Position.Y + Math.Cos(randomValue)*plant.SeedSpreadingRadius),
+                                                 0.0f);
+                world.Objects.Add(childPlant);
+            }
         }
     }
 }
