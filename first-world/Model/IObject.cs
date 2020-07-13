939febf3b1a9ebe173267dbbffd34384189ed40b
Yang Zhang <yangzhang1908@gmail.com>
2020-07-13 04:06:44 +0200

    Added breed method  for IObjects

--- first-world/Model/IObject.cs
+++ first-world/Model/IObject.cs
@@ -1,8 +1,14 @@
+using System.Numerics;
+
 namespace FirstWorld.Model
 {
     public interface IObject
     {
         public long Age { set; get; }
         public long MaxAge { set; get; }
+        public Vector3 Position { set; get; }
+        public long BreedingAge { set; get; }
+        public long BreedingPeriod { set; get; }
+        public int MaxBreedingNumber { set; get; }
     }
 }
