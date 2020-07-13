9afeb87daca0277be5a6fb28b202a4cd4cd6985d
Yang Zhang <yangzhang1908@gmail.com>
2020-07-13 04:04:49 +0200

    Added Breed method for plants

--- first-world/Model/Plant.cs
+++ first-world/Model/Plant.cs
@@ -12,5 +12,10 @@ namespace FirstWorld.Model
         public long BreedingAge { set; get; }
         public long BreedingPeriod { set; get; }
         public float ChildSpreadingRadius { set; get; }
+        public IObject Breed(){
+            IObject childPlant = new Plant();
+            return childPlant;
+            }
+        //public Plant Breed { set; get; }
     }
 }
